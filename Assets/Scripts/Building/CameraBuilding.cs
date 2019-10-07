2403b5e2d37de590fb1b0daf3e74ebebec947068
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2019-10-07 22:15:00 +0100

    :sparkles: : instantiate prefabs

--- Assets/Scripts/Building/CameraBuilding.cs
+++ Assets/Scripts/Building/CameraBuilding.cs
@@ -8,53 +8,75 @@ namespace com.hermitGames.rp
     public class CameraBuilding : MonoBehaviour
     {
         [SerializeField] private GameObject prefab;
-        [SerializeField] private Color virtualColor;
-        [SerializeField] private float speed = 30f;
+        [SerializeField] private float unitCellSize = 2f;
+        [SerializeField] private float moveSpeed = 30f;
+        [SerializeField] private float zoomSpeed = 30f;
 
         private new Camera camera;
 
         private float moveX;
         private float moveY;
+        private float moveZ;
 
         private GameObject selectedPrefab;
-        private Color originalPrefabColor;
+        private PreviewState previewState;
 
         // Start is called before the first frame update
         void Start() {
-            this.camera = GetComponent<Camera>();
+            this.camera = GetComponent<Camera>();  
+        }
 
+        private void OnEnable() {
             this.selectedPrefab = Instantiate(this.prefab);
+            this.selectedPrefab.AddComponent<Preview>();
             this.selectedPrefab.SetActive(false);
-            this.originalPrefabColor = this.selectedPrefab.GetComponent<Renderer>().material.color;
-            this.selectedPrefab.GetComponent<Renderer>().material.color = this.virtualColor;
+            Preview.statusChangedEvent += this.PreviewStateChanged;
+        }
+
+        private void OnDisable() {
+            Preview.statusChangedEvent -= this.PreviewStateChanged;
+            Destroy(this.selectedPrefab);
+        }
+
+        private void PreviewStateChanged(PreviewState state) {
+            Debug.Log(state);
+            this.previewState = state;
         }
 
         // Update is called once per frame
         void Update() {
             this.moveX = Input.GetAxis("Horizontal");
             this.moveY = Input.GetAxis("Vertical");
+            this.moveZ = Input.GetAxis("Mouse ScrollWheel");
+
+            if (Input.GetMouseButtonDown(1)) {
+                this.selectedPrefab.transform.Rotate(new Vector3(0, 90f, 0));
+            }
 
             Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             // Raycast only on terrain
-            if (Physics.Raycast(ray, out hit, 100, (1 << 8))) {
-                if(this.selectedPrefab) {
+            if (Physics.Raycast(ray, out hit, 100f)) {
+                if (this.selectedPrefab) {
                     this.selectedPrefab.SetActive(true);
-                    this.selectedPrefab.transform.position = hit.point;
+                    this.selectedPrefab.transform.position = this.GetNearestPoint(hit.point);
 
                     if (Input.GetMouseButtonDown(0)) {
-                        this.selectedPrefab.GetComponent<Renderer>().material.color = this.originalPrefabColor;
-                        this.selectedPrefab = null;
+                        GameManager.instance.CmdRegisterEntity(this.prefab, this.selectedPrefab.transform.position, this.selectedPrefab.transform.rotation.eulerAngles);
                     }
-                }         
-            } else if(this.selectedPrefab){
+                }
+            } else if (this.selectedPrefab) {
                 this.selectedPrefab.SetActive(false);
             }
         }
 
         private void FixedUpdate() {
-            this.transform.Translate(this.moveX * this.speed * Time.deltaTime, this.moveY * this.speed * Time.deltaTime, 0);
+            this.transform.Translate(this.moveX * this.moveSpeed * Time.deltaTime, this.moveY * this.moveSpeed * Time.deltaTime, this.moveZ * this.zoomSpeed * Time.deltaTime);
+        }
+
+        private Vector3 GetNearestPoint(Vector3 pos) {
+            return new Vector3(Mathf.RoundToInt(pos.x / this.unitCellSize) * this.unitCellSize, 0, Mathf.RoundToInt(pos.z / this.unitCellSize) * this.unitCellSize);
         }
     }
 }
---
+++ Assets/Scripts/Building/Preview.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.hermitGames.rp
+{
+    public class Preview : MonoBehaviour
+    {
+        public delegate void StatusChanged(PreviewState state);
+        public static event StatusChanged statusChangedEvent;
+
+        private Color virtualColorGreen = new Color(0, 1, 0, 0.4f);
+        private Color virtualColorRed = new Color(1, 0, 0, 0.4f);
+        private new Renderer renderer;
+        private PreviewState state;
+
+        // Start is called before the first frame update
+        void Start() {
+            this.renderer = GetComponent<Renderer>();
+
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            if (rigidbody == null) {
+                rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            }
+
+            rigidbody.useGravity = false;
+            rigidbody.isKinematic = true;
+
+            this.renderer.material.color = this.virtualColorGreen;
+
+            foreach (Collider coll in GetComponentsInChildren<Collider>()) {
+                coll.isTrigger = true;
+            }
+        }
+
+        private void OnTriggerStay(Collider other) {
+            if (other.gameObject.name != "Terrain") {
+                if (this.state == PreviewState.VALID) {
+                    this.state = PreviewState.NOT_VALID;
+                    statusChangedEvent(this.state);
+                    this.renderer.material.color = this.virtualColorRed;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (state == PreviewState.NOT_VALID) {
+                this.state = PreviewState.VALID;
+                statusChangedEvent(this.state);
+                this.renderer.material.color = this.virtualColorGreen;
+            }
+        }
+    }
+
+    public enum PreviewState
+    {
+        NOT_VALID,
+        VALID
+    }
+}
+
