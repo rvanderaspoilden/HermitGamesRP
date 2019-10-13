1fe0e670b43f2cb4e073670907a56f5d1df7cc05
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2019-10-13 18:47:36 +0100

    :sparkles: : build v1 works

---
+++ Assets/Scripts/Manager/BuildUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace com.hermitGames.rp
+{
+    public class BuildUIManager : MonoBehaviour
+    {
+        [SerializeField] private Transform scrollviewContent;
+        [SerializeField] private GameObject prefabButtonOption;
+
+        public static BuildUIManager instance;
+
+        // Start is called before the first frame update
+        void Start() {
+            instance = this;
+        }
+
+        public void Init() {
+            foreach(GameObject prefab in GameManager.prefabDatabase.Values) {
+                GameObject prefabOption = Instantiate(this.prefabButtonOption, this.scrollviewContent);
+                prefabOption.GetComponentInChildren<TextMeshProUGUI>().text = prefab.name;
+            }
+        }
+    }
+}
